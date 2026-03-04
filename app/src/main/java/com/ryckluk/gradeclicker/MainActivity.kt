86f4a338e1e1b9f531244d82913b3b161e702524
okolodk <grisha.zhivot@bk.ru>
2026-03-04 20:00:53 +0300

    Step 5: Basic model and file

--- app/src/main/java/com/ryckluk/gradeclicker/MainActivity.kt
+++ app/src/main/java/com/ryckluk/gradeclicker/MainActivity.kt
@@ -19,12 +19,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Grade_ClickerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+
+
             }
         }
     }
---
+++ app/src/main/java/com/ryckluk/gradeclicker/model/Grade.kt
@@ -0,0 +1,9 @@
+package com.ryckluk.gradeclicker.model
+
+import androidx.annotation.DrawableRes
+
+data class Grade(
+    @DrawableRes  val imageId: Int,
+    val pointsPerClick: Int,
+    val threshold: Int
+)
