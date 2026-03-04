244dc4a4f6205c72266546df4d2955258e1068f3
okolodk <grisha.zhivot@bk.ru>
2026-03-04 20:44:21 +0300

    Step 7: Basic Grade Clicker app completed

--- app/src/main/java/com/ryckluk/gradeclicker/MainActivity.kt
+++ app/src/main/java/com/ryckluk/gradeclicker/MainActivity.kt
@@ -4,14 +4,42 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import com.ryckluk.gradeclicker.model.Grade
+import com.ryckluk.gradeclicker.ui.theme.Grade_ClickerTheme
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.material3.Text
+import androidx.compose.foundation.Image
+import androidx.compose.material3.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.graphics.TransformOrigin
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.ryckluk.gradeclicker.ui.theme.Grade_ClickerTheme
+import androidx.compose.ui.unit.dp
+import com.ryckluk.gradeclicker.data.Datasource
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,10 +47,105 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Grade_ClickerTheme {
-
+                Surface(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    GradeClickerApp(grades = Datasource.gradeList)
+                }
 
             }
         }
     }
 }
 
+@Composable
+fun GradeClickerApp(grades: List<Grade>) {
+    var points by rememberSaveable { mutableStateOf(0) }
+    var clicks by rememberSaveable { mutableStateOf(0) }
+    val currentGrade = determineGradeToShow(grades, points)
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(dimensionResource(R.dimen.padding_medium)),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(
+            text = stringResource(R.string.app_name),
+            style = MaterialTheme.typography.headlineLarge,
+            modifier = Modifier.padding(16.dp)
+        )
+        Image(
+            painter = painterResource(currentGrade.imageId),
+            contentDescription = null,
+            modifier = Modifier
+                .size(dimensionResource(R.dimen.image_size))
+                .clickable{
+                    points += currentGrade.pointsPerClick
+                    clicks++
+                },
+            contentScale = ContentScale.Fit
+        )
+        TransactionInfo(
+            points = points,
+            clicks = clicks
+        )
+    }
+}
+
+@Composable
+fun TransactionInfo(
+    points: Int,
+    clicks: Int,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Row(modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(text = stringResource(R.string.points_earned),
+                style = MaterialTheme.typography.titleMedium)
+            Text(
+                text = points.toString(),
+                style = MaterialTheme.typography.titleMedium
+            )
+        }
+        Spacer(modifier = Modifier.height(8.dp))
+        Row(modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween) {
+            Text(
+                text = stringResource(R.string.total_clicks),
+                style = MaterialTheme.typography.bodyLarge
+            )
+            Text(
+                text = clicks.toString(),
+                style = MaterialTheme.typography.bodyLarge
+            )
+        }
+    }
+}
+
+
+fun determineGradeToShow(grades: List<Grade>, points : Int) : Grade {
+    var gradeToShow = grades.first()
+    for (grade in grades) {
+        if (points >= grade.threshold){
+            gradeToShow = grade
+        } else{
+            break
+        }
+    }
+    return gradeToShow
+}
+
+@Preview
+@Composable
+fun GradeClickerPreview(){
+    Grade_ClickerTheme {
+        GradeClickerApp(grades = Datasource.gradeList)
+    }
+}
